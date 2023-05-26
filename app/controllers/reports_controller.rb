afa3d062c82a4630f7c78d34448b06bbfa84743b
Stif <stiven.maldonado@symplifica.com>
2023-05-25 20:24:57 -0500

    Solution

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -5,6 +5,18 @@ class ReportsController < ApplicationController
     unless validate_params?
       render :json => { :errors => "validate" }, :status => 400
     end
+    get_filter
+    get_problems
+    @total_hours = 0.0
+    get_working_hrs
+
+    render :json => {
+      employee_id: params[:employee_id].to_i,
+      from: params[:from], to: params[:to],
+      working_hrs: @total_hours.round(2),
+      problematic_dates: @problematic_dates
+    }, :status => 200
+
   end
 
   def validate_params?
@@ -14,4 +26,35 @@ class ReportsController < ApplicationController
     report.to = params[:to]
     report.valid?
   end
+
+  private
+
+  def get_filter
+    @filter_event = Event.where(timestamp: ("#{params[:from]} 01:00".."#{params[:to]} 23:59"), employee_id: params[:employee_id].to_i)
+                         .map { |x| { date: x.timestamp.to_date, kind: x.kind, time: x.timestamp } }
+                         .group_by { |td| td[:date] }
+  end
+
+  def get_problems
+    @problematic_dates = []
+    @filter_event.each do |filter|
+      there_in = filter[1].any? { |x| x[:kind] == "in" }
+      there_out = filter[1].any? { |x| x[:kind] == "out" }
+
+      if !there_in || !there_out
+        @problematic_dates << filter[0].strftime("%Y-%m-%d")
+      end
+    end
+  end
+
+  def get_working_hrs
+    @filter_event.each do |filter|
+      order = filter[1].sort_by { |c| c[:time] }
+      init_work = order.first[:time]
+      end_work = order.last[:time]
+      @total_hours += ((end_work.time - init_work) / 3600)
+    end
+    @total_hours
+  end
+
 end
--- spec/api_spec.rb
+++ spec/api_spec.rb
@@ -10,10 +10,11 @@ RSpec.describe 'public api', type: :request do
   let(:second_employee) { 2 }
 
   it 'generates report based on events' do
-    given_event(employee_id: first_employee, timestamp: '2019-05-01 08:00', kind: :in)
-    given_event(employee_id: first_employee, timestamp: '2019-05-01 16:00', kind: :out)
-    given_event(employee_id: first_employee, timestamp: '2019-05-02 08:00', kind: :in)
-    given_event(employee_id: first_employee, timestamp: '2019-05-02 16:00', kind: :out)
+
+    post_event(employee_id: first_employee, timestamp: '2019-05-01 08:00', kind: :in)
+    post_event(employee_id: first_employee, timestamp: '2019-05-01 16:00', kind: :out)
+    post_event(employee_id: first_employee, timestamp: '2019-05-02 08:00', kind: :in)
+    post_event(employee_id: first_employee, timestamp: '2019-05-02 16:00', kind: :out)
 
     report = report(employee_id: first_employee, from: '2019-05-01', to: '2019-05-30')
 
@@ -26,8 +27,9 @@ RSpec.describe 'public api', type: :request do
   end
 
   it 'rounds work time to two decimal places' do
-    given_event(employee_id: first_employee, timestamp: '2019-05-01 08:10:00:050', kind: :in)
-    given_event(employee_id: first_employee, timestamp: '2019-05-01 16:00:01:070', kind: :out)
+
+    post_event(employee_id: first_employee, timestamp: '2019-05-01 08:10:00:050', kind: :in)
+    post_event(employee_id: first_employee, timestamp: '2019-05-01 16:00:01:070', kind: :out)
 
     report = report(employee_id: first_employee, from: '2019-05-01', to: '2019-05-01')
 
@@ -35,9 +37,9 @@ RSpec.describe 'public api', type: :request do
   end
 
   it 'discovers problematic date when there is no leave event' do
-    given_event(employee_id: first_employee, timestamp: '2019-05-01 08:00', kind: :in)
-    given_event(employee_id: first_employee, timestamp: '2019-05-02 08:00', kind: :in)
-    given_event(employee_id: first_employee, timestamp: '2019-05-02 16:00', kind: :out)
+    post_event(employee_id: first_employee, timestamp: '2019-05-01 08:00', kind: :in)
+    post_event(employee_id: first_employee, timestamp: '2019-05-02 08:00', kind: :in)
+    post_event(employee_id: first_employee, timestamp: '2019-05-02 16:00', kind: :out)
 
     report = report(employee_id: first_employee, from: '2019-05-01', to: '2019-05-30')
 
@@ -50,8 +52,8 @@ RSpec.describe 'public api', type: :request do
   end
 
   it 'discovers problematic date when there is skip day event' do
-    given_event(employee_id: first_employee, timestamp: '2019-05-01 08:00', kind: :in)
-    given_event(employee_id: first_employee, timestamp: '2019-05-02 16:00', kind: :out)
+    post_event(employee_id: first_employee, timestamp: '2019-05-01 08:00', kind: :in)
+    post_event(employee_id: first_employee, timestamp: '2019-05-02 16:00', kind: :out)
 
     report = report(employee_id: first_employee, from: '2019-05-01', to: '2019-05-30')
 
